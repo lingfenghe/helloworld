706023fd67b1c58e26367084fd648510913e4f10
lingfenghe <lingfenghe@webank.com>
2020-02-25 20:58:36 +0800

    fix: change java file to add 1 more line again

--- src/com/helloworld/experiment/ExperimentDemo.java
+++ src/com/helloworld/experiment/ExperimentDemo.java
@@ -5,6 +5,7 @@ public class ExperimentDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Github");
+		System.out.println("Hello Github Again");
 	}
 
 }
