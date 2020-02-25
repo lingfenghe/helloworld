71f123b0e07603a33d0b0ac2d67d52200ca84136
lingfenghe <lingfenghe@webank.com>
2020-02-25 21:21:41 +0800

    remove one line from java

--- src/com/helloworld/experiment/ExperimentDemo.java
+++ src/com/helloworld/experiment/ExperimentDemo.java
@@ -5,7 +5,6 @@ public class ExperimentDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Github");
-		System.out.println("Hello Github Again");
 	}
 
 }
