af90c4c00871d1f93eda17be9c838887a0ab20c2
lingfenghe <lingfenghe@webank.com>
2020-02-26 14:45:46 +0800

    change java on develop branch

--- src/com/helloworld/experiment/ExperimentDemo.java
+++ src/com/helloworld/experiment/ExperimentDemo.java
@@ -3,8 +3,8 @@ package com.helloworld.experiment;
 public class ExperimentDemo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Hello Github");
+		System.out.println("Hello World, Master");
+		System.out.println("Hello World, Develop");
 	}
 
 }
